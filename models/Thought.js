696ff639a30156d776a94005a7a2122acf483cdd
Zoe Rorvig <zrorvig@gmail.com>
2023-01-31 23:20:16 -0500

    updated thoughts model to reflect reactions

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,32 @@
 const { Schema, model } = require("mongoose");
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -15,15 +42,21 @@ const thoughtSchema = new Schema(
         username: {
             type: String,
             required: true,
-        }
+        },
+        reactions: [reactionSchema]
     },
     {
         toJSON: {
             getters: true,
+            virtuals: true,
         },
         id: false,
     }
 );
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
 
 const Thought = model("Thought", thoughtSchema);
 
